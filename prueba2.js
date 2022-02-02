b3ce8dbbef80235aa23e5345058e69e4328b3f84
niclosprog <nicocprog@gmail.com>
2022-02-02 01:51:06 -0300

    cambios nuevos

--- prueba2.js
+++ prueba2.js
@@ -1,9 +1,33 @@
 var EdadFrancisco = 24;
 var EdadNicolas = 22;
 
+/*
 if (EdadFrancisco > EdadNicolas) {
 
     var diferenciaEdad = (EdadFrancisco - EdadNicolas);
     console.log(`Francisco es mayor que Nicolas por ${diferenciaEdad} años`);
 
 }
+*/
+var marca;
+var bebida;
+
+function escabiar(plata,bebida) {
+    if (plata > 850 && bebida === 'Fernet') {
+        marca = 'Branca';
+        console.log(`Vamos a comprar ${bebida} marca ${marca}`);
+    } else if (plata > 2400 && bebida === 'Vodka') {
+        marca = 'Absolut';
+        console.log(`Vamos a comprar ${bebida} marca ${marca}`);
+    } else if (plata < 2400 && bebida === 'Vodka') {
+        marca = 'Smirnoff';
+        console.log(`Vamos a comprar ${bebida} marca ${marca}`);
+    } else if (plata < 850 && bebida === 'Fernet') {
+        marca = 'Vittone';
+        console.log(`Vamos a comprar ${bebida} marca ${marca}`);
+    } else {
+        console.log('No vamos a escabiar un pingo');
+    }
+}
+
+escabiar(400,'Fernet');
